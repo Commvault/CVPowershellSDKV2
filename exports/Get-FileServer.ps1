67537aa71e0637659496336eed0990bc590e32b1
JnaneshD05 <97098752+JnaneshD05@users.noreply.github.com>
2022-03-06 18:06:14 +0530

    Added examples

--- exports/Get-FileServer.ps1
+++ exports/Get-FileServer.ps1
@@ -19,13 +19,28 @@ This endpoint is used to return the list of file servers.
 .Description
 This endpoint is used to return the list of file servers.
 .Example
-PS C:\> {{ Add code here }}
+PS C:\> Get-FileServer | Format-List
 
-{{ Add output here }}
+FileServerCount : 12
+FileServers     : {JNANESH_CS_1, jnan_test_bdc, jnan_linux_3, jnancs_sp19_3…}
+
 .Example
-PS C:\> {{ Add code here }}
+PS C:\> Get-FileServer | Select-object -ExpandProperty FileServers
 
-{{ Add output here }}
+ApplicationSize CommcellName Configured DisplayName     Guid                                 Id  Name            Status         Type
+--------------- ------------ ---------- -----------     ----                                 --  ----            ------         ----
+0                            True       jnanesh_cs_1    7861B9C2-661E-4CF3-A40E-C354F741E30A 2   JNANESH_CS_1    ONLINE         WINDOWS
+0                            True       jnan_test_bdc   F08E2B1C-1857-4CDC-91A0-E6F30F3F8444 11  jnan_test_bdc   ONLINE         WINDOWS
+0                            True       jnan_linux_3    922EB4D1-0BB8-475B-8FC7-BDED92410ABF 19  jnan_linux_3    ONLINE         UNIX
+0                            True       jnancs_sp19_3   6BD14B11-57AA-4272-A447-A441B47DEBD1 56  jnancs_sp19_3   ONLINE         WINDOWS
+3695499                      True       bdcdemo2_client 41DEB9CE-C715-4001-B877-EF86F24B75C5 86  bdcdemo2_client ONLINE         WINDOWS
+0                            True       dockerwc        27FEAF97-56EC-4B5B-BECA-13A946210EB7 88  dockerwc        OFFLINE        WINDOWS
+282477535                    True       Client2         0C3523CB-6F19-477E-9FDD-16F3F7780D7E 299 Client2         ONLINE         WINDOWS
+0                            True       Client4         7F461D2D-BA53-4B46-8E08-56E8CA154663 300 Client4         ONLINE         WINDOWS
+407652014                    True       Client3         B05F2D7F-9256-4424-B170-C30394038059 301 Client3         ONLINE         WINDOWS
+26                           True       Client7         F6B00258-655B-4394-904A-1748CAEC7B52 305 Client7         OFFLINE        WINDOWS
+0                            True       172.19.71.58    A6D635FB-7A7C-41BA-B1DD-7195AF6BB8BC 336 172.19.71.58    NOT_APPLICABLE NAS
+0                            True       nfs_server_name 8DCB5C19-92B9-49B9-BB3B-4FC4DAD9A26B 337 nfs_server_name NOT_APPLICABLE NAS
 
 .Outputs
 Commvault.Powershell.Models.IFileServerListResp
